175bb2ac29ae0d51160e2e9c5d65b26e1cc04ab6
Botir <117967765+AbdualimovBotir@users.noreply.github.com>
2024-12-07 13:49:10 +0500

    Create flask_app.py
    1

---
+++ flask_app.py
@@ -0,0 +1,50 @@
+from flask import Flask, Blueprint, request, jsonify, render_template, send_from_directory
+import pickle
+
+# Flask ilovasini yaratish
+app = Flask(__name__)
+
+# Model va vektorizerni yuklash
+with open('models/sentiment_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('models/vectorizer.pkl', 'rb') as vec_file:
+    vectorizer = pickle.load(vec_file)
+
+# Flask Blueprint
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('index.html')
+
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
+
+@main.route('/predict', methods=['POST'])
+def predict():
+    # Foydalanuvchi tomonidan yuborilgan matnni olish
+    data = request.get_json()
+    
+    if 'text' not in data:
+        return jsonify({'error': 'No text provided!'}), 400
+
+    text = data['text']
+    
+    # Matnni vektorlashtirish
+    text_vectorized = vectorizer.transform([text])
+    
+    # Sentimentni bashorat qilish
+    prediction = model.predict(text_vectorized)[0]
+    sentiment = 'Positive' if prediction == 1 else 'Negative'
+
+    # Javobni yuborish
+    return jsonify({'text': text, 'sentiment': sentiment})
+
+# Blueprintni asosiy ilovaga qo'shish
+app.register_blueprint(main)
+
+# Flask ilovasini ishga tushirish
+if __name__ == "__main__":
+    app.run(debug=False, port=5000)  # Flask ilovasini 5000-portda ishga tushirish
